2573f19d5560d91695ff7a6efce9440dcd518d92
Alexander <sanaikulikov1@mail.ru>
2023-03-24 23:03:51 +0300

    release 1

--- sudoku.js
+++ sudoku.js
@@ -1,31 +1,44 @@
-/**
- * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
- * Возвращает игровое поле после попытки его решить.
- * Договорись со своей командой, в каком формате возвращать этот результат.
- */
-function solve(boardString) {
+let solveSudoku = function(board) {
+  const size = 9;
+  const boxSize = 3;
+//пустые строки 
+function voidd (board) {
+  for (let i = 0; i < size; i++) {
+      for (let c = 0; c < size; c++) {
+          if(board[i][c] === '-') {
+              return [i,c];
+          }
+      }
+  }
+  return '0';
 }
+// сюда вставить функцию valid которую скинет ярик
 
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает булевое значение — решено это игровое поле или нет.
- */
-function isSolved(board) {
+// 
+const solve = () => {
+  const currPos = voidd(board);
+  if (currPos === '0') {
+      return true;
+  }
+  for (let i = 1; i < size + 1; i++) {
+    
+      const currNum = i.toString();
+      const isValid = valid(currNum, currPos, board);
+    
+      if (isValid) {
+          const [x,y] = currPos;
+          board[x][y] = currNum;
 
+          if(solve()) {
+              return true;
+          }
+
+          board[x][y] = '-';
+      }
+  }
+
+  return false;
 }
-
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает строку с игровым полем для последующего вывода в консоль.
- * Подумай, как симпатичнее сформировать эту строку.
- */
-function prettyBoard(board) {
-
-}
-
-// Экспортировать функции для использования в другом файле (например, readAndSolve.js).
-module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
+solve();
+return board;
 };
